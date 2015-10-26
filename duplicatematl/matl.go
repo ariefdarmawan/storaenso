ef92b681a94ca1b0711a09e72e2fc88d81d241ce
juragan360 <ariefda@hotmail.com>
2015-10-27 06:25:22 +0700

    simplify material poulate

--- duplicatematl/matl.go
+++ duplicatematl/matl.go
@@ -14,20 +14,6 @@ func PopulateMaterial() []toolkit.M {
 	mats := make([]toolkit.M, 0)
 	csr := DbCtx().Connection.Query().From("MaintenancePart").Select("_id", "Matnr", "Trimmed").Cursor(nil)
 	csr.SetPooling(true)
-	//var b bool
-
-	//fmt.Println("Populate data")
-	//var _ error
-	/*
-		m := new(toolkit.M)
-		b = true
-		iRead := 0
-		for ; b == true; b, _ = csr.Fetch(&m) {
-			iRead++
-			mats = append(mats, *m)
-			//fmt.Printf("Reading %d data \n", iRead)
-		}
-	*/
 	csr.FetchAll(&mats, false)
 	return mats
 }
